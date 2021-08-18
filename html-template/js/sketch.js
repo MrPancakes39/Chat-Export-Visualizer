80907949b9dcc2d08c5ebdf9b34125b2118b282d
MrPancakes39 <salmannhassan39@gmail.com>
2021-08-18 19:15:58 +0300

    ♻️ Refactored genMsg function

--- html-template/js/sketch.js
+++ html-template/js/sketch.js
@@ -28,7 +28,7 @@ function validURL(str) {
         '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
         '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
     try {
-    return !!pattern.test(str);
+        return !!pattern.test(str);
     } catch (e) {
         return false;
     }
@@ -50,7 +50,7 @@ function typeOfMsg(msg) {
 
 // Gets date in format "day month_name year".
 function getDate(date) {
-    a = moment(date).format("LL").split(" ");
+    let a = moment(date).format("LL").split(" ");
     [a[0], a[1]] = [a[1].slice(0, -1), a[0]];
     return a.join(" ");
 }
@@ -58,91 +58,112 @@ function getDate(date) {
 // Gets time in format "xx:xx am/pm".
 const getTime = (date) => moment(date).format("LT").toLowerCase();
 
+// Creates message-row div.
+function createMsg(msgObj) {
+    let type = typeOfMsg(msgObj);
+
+    // parent > message-row > message-text
+    let div = document.createElement("div"); // Creates a message-row container.
+    div.className = `message-row ${type}`;
+
+    let msgText = document.createElement("div"); // Creates a message-text container.
+    msgText.className = "message-text";
+
+    if (type.includes("system")) {
+        msgText.innerText = msgObj["message"];
+    } else {
+        let msg = msgObj["message"].trim();
+
+        /*
+         *  Creates 4 containers:
+         *  message-text > (author, container)
+         *  container > (text, time)
+         */
+        let text = document.createElement("p");
+        let time = document.createElement("p");
+        let author = document.createElement("p");
+        let container = document.createElement("div");
+
+        text.className = "text";
+        time.className = "time";
+        author.className = "author";
+        container.className = "container";
+
+        time.innerText = getTime(msgObj["date"]);
+
+        if (groupChat && type.includes("other"))
+            author.innerText = msgObj["author"].trim();
+
+        // If msg is valid url create <a> tag for it.
+        if (validURL(msg)) {
+            let a = document.createElement("a");
+            // Checks if the URL starts with https if not add it.
+            a.href = (msg.match(/(^https?:\/\/)/g)) ? msg : `https://${msg}`;
+            a.target = "_blank";
+            a.innerText = msg;
+            text.appendChild(a);
+        } else {
+            // if <Media omitted> then we don't emojione it.
+            if (msg == "<Media omitted>") {
+                text.innerText = msg;
+            } else {
+                text.innerHTML = emojione.toImage(msg);
+            }
+        }
+
+        container.append(text, time);
+        msgText.append(author, container);
+    }
+
+    div.appendChild(msgText);
+    return div;
+}
+
+// Creates a date message-row.
+function createDateMsg(msgObj) {
+    // If date changes add a date message row.
+    nextDate = getDate(msgObj["date"]);
+    if (currentDate != nextDate) {
+        let dateDiv = document.createElement("div");
+        dateDiv.className = "message-row system-message date";
+
+        let dateText = document.createElement("div");
+        dateText.className = "message-text";
+
+        dateText.innerText = nextDate;
+        currentDate = nextDate;
+
+        dateDiv.appendChild(dateText);
+        return dateDiv;
+    }
+    return null;
+}
+
 async function genMsg() {
     data = await getData();
     let parent = document.querySelector("#messages"); // Selects parent div.
     for (let i = 0; i < data.length; i++) {
-        let type = typeOfMsg(data[i]);
+        // Creates message-row div.
+        let div = createMsg(data[i]);
 
-        // parent > message-row > message-text
-        let div = document.createElement("div"); // Creates a message-row container.
-        if (i > 0 && type == "you-message") {
+        // Sets style for you-message div.
+        if (i > 0 && typeOfMsg(data[i]) == "you-message") {
             let typeBefore = typeOfMsg(data[i - 1]);
             let typeAfter = typeOfMsg(data[i + 1]);
 
-            type =
+            let type =
                 (typeBefore != "you-message" && typeAfter == "you-message") ? "you-message start" :
                 (typeBefore == "you-message" && typeAfter == "you-message") ? "you-message middle" :
                 (typeBefore == "you-message" && typeAfter != "you-message") ? "you-message last" :
                 "you-message";
-        }
-        div.className = `message-row ${type}`;
 
-        let msgText = document.createElement("div"); // Creates a message-text container.
-        msgText.className = "message-text";
-
-        if (type.includes("system")) {
-            msgText.innerText = data[i]["message"];
-        } else {
-            let msg = data[i]["message"].trim();
-
-            /*
-             *  Creates 4 containers:
-             *  message-text > (author, container)
-             *  container > (text, time)
-             */
-            let text = document.createElement("p");
-            let time = document.createElement("p");
-            let author = document.createElement("p");
-            let container = document.createElement("div");
-
-            text.className = "text";
-            time.className = "time";
-            author.className = "author";
-            container.className = "container";
-
-            time.innerText = getTime(data[i]["date"]);
-
-            if (groupChat && type.includes("other"))
-                author.innerText = data[i]["author"].trim();
-
-            // If msg is valid url create <a> tag for it.
-            if (validURL(msg)) {
-                let a = document.createElement("a");
-                a.href = (msg.match(/(^https?:\/\/)/g)) ? msg : `https://${msg}`;
-                a.target = "_blank";
-                a.innerText = msg;
-                text.appendChild(a);
-            } else {
-                // if <Media omitted> then we emojione it.
-                if (msg == "<Media omitted>") {
-                    text.innerText = msg;
-                } else {
-                    text.innerHTML = emojione.toImage(msg);
-                }
-            }
-
-            container.append(text, time);
-            msgText.append(author, container);
+            div.className = `message-row ${type}`;
         }
 
-        // If date changes add a date message row.
-        nextDate = getDate(data[i]["date"]);
-        if (currentDate != nextDate) {
-            let dateDiv = document.createElement("div");
-            dateDiv.className = "message-row system-message date";
-
-            let dateText = document.createElement("div");
-            dateText.className = "message-text";
-
-            dateText.innerText = nextDate;
-            currentDate = nextDate;
-
-            dateDiv.appendChild(dateText);
+        let dateDiv = createDateMsg(data[i]);
+        if (dateDiv)
             parent.appendChild(dateDiv);
-        }
 
-        div.appendChild(msgText);
         parent.appendChild(div);
     }
 }
