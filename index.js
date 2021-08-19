ee9bc3d649ab0b25fe43d0094f12e52ce32e772d
MrPancakes39 <salmannhassan39@gmail.com>
2021-08-19 23:32:42 +0300

    🚧 Changed How Messages are Created
    - Now Messages Array are split into 1000 messages chunks.
    - The number of chunks are the number of pages.
    - Didn't Implement pages UI yet.
    - Now displays a loader while waiting for messages to be created.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
+const chunk = require("lodash.chunk");
 const waParser = require("whatsapp-chat-parser").parseString;
 
 async function parseWAChat(inputFile, outputFile) {
     const raw_data = fs.readFileSync(inputFile, "utf-8");
     const messages = await waParser(raw_data);
-    fs.writeFile(outputFile, JSON.stringify(messages), "utf-8", (err) => { if (err) return console.error(err) });
+    message_pages = chunk(messages, 1000);
+    fs.writeFile(outputFile, JSON.stringify(message_pages), "utf-8", (err) => { if (err) return console.error(err) });
 }
 
 parseWAChat("./WhatsApp Chat.txt", "./messages.json");
